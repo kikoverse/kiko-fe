48cf75cb429a2d236c4e46696ab1dcc2c15b0dcc
xiaziheng <xiaziheng@aixuexi.com>
2022-09-05 15:31:38 +0800

    feat： add style

--- src/views/AptosPreheat/Index.vue
+++ src/views/AptosPreheat/Index.vue
@@ -9,25 +9,32 @@
     >
       <div :class="$style['img-box']">
         <img :src="item.boxUrl" alt="" />
+        <div :class="$style['btn']">
+          <FlyButton @click="submit">
+            <div>{{ $t("领取") }}</div>
+          </FlyButton>
+        </div>
       </div>
       <div :class="$style['item-content']">
-        <span>{{
-          store.state.StoreApp.currLang === "en"
-            ? `${item.enName}`
-            : store.state.StoreApp.currLang === "zh"
-            ? `${item.cnName}`
-            : ""
-        }}</span>
-        <div
-          :class="$style['item-desc']"
-          v-html="
-            store.state.StoreApp.currLang === 'en'
-              ? item.enRule
-              : store.state.StoreApp.currLang === 'zh'
-              ? item.cnRule
-              : ''
-          "
-        ></div>
+        <div :class="$style['descBox']">
+          <span>{{
+            store.state.StoreApp.currLang === "en"
+              ? `${item.enName}`
+              : store.state.StoreApp.currLang === "zh"
+              ? `${item.cnName}`
+              : ""
+          }}</span>
+          <div
+            :class="$style['item-desc']"
+            v-html="
+              store.state.StoreApp.currLang === 'en'
+                ? item.enRule
+                : store.state.StoreApp.currLang === 'zh'
+                ? item.cnRule
+                : ''
+            "
+          ></div>
+        </div>
       </div>
     </div>
   </div>
@@ -46,6 +53,7 @@ import { getAirdropList } from "../../api/active";
 import { WALLET_DIALOG_PARAMS } from "../../constants/dialog";
 import { getContractWidthSingerAddress } from "../../walletUtils/stcContract";
 import FlyWalletDialog from "@FlyUI/FlyWalletDialog.vue";
+import FlyButton from "../../FlyUI/FlyButton.vue";
 
 const store = useStore();
 const state = reactive({
@@ -105,9 +113,19 @@ $black: #010e22;
   margin-bottom: 20px;
   overflow: hidden;
   margin-right: 28px;
+  position: relative;
+  cursor: pointer;
   &:hover {
     box-shadow: 0 18px 32px -18px rgba(0, 0, 0, 0.6) !important;
     transform: translateY(-3px);
+    .img-box {
+      // .descBox {
+      //   opacity: 0;
+      // }
+      .btn {
+        opacity: 1;
+      }
+    }
   }
   .img-box {
     width: 100%;
@@ -126,10 +144,24 @@ $black: #010e22;
       border-top-left-radius: $border-radius;
       border-top-right-radius: $border-radius;
     }
+    .btn {
+      position: absolute;
+      transition: 0.3s all ease;
+      left: 0;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      opacity: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background: rgba(0, 0, 0, 0.3);
+    }
   }
   .item-content {
     padding: 12px 16px 24px 16px;
     color: #fff;
+
     .category {
       display: block;
       background: rgba(251, 168, 0, 0.3);
@@ -143,6 +175,10 @@ $black: #010e22;
       margin-top: 5px;
       font-size: 12px;
       color: #fba800;
+    }
+
+    .descBox {
+      display: block;
     }
   }
 }
