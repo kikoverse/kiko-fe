569f95eff2c716c6dc7a3d279f001dec8551f0f6
xiaziheng <xiaziheng@aixuexi.com>
2022-10-19 16:36:38 +0800

    feat: add

---
+++ src/views/test/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="aaa">connect</button>
+</template>
+<script setup>
+// import { getMartianAccountBalance } from "../../walletUtils/martianMask";
+// import { AptosClient } from "aptos";
+const aaa = async () => {
+  await window.martian.connect();
+  const metadata = {
+    address: true,
+    application: true,
+    chainId: true,
+    message: "test aptos secp256k1",
+    nonce: 12345,
+  };
+  const signature = await window.martian.signMessage(metadata);
+  console.log(JSON.stringify(signature));
+};
+</script>
